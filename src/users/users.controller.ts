80ea3dd20a7bc693f3680479f8561db1b4f730a3
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-09-15 19:41:39 +0530

    writing controllers (routes)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Delete } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+   @Get()
+   findAll(): string {
+      return "This function returns detils of users"
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return `Giving details of user ${id}`
+   }
+
+   @Post()
+   create(@Body() user: {}) {
+      return user
+   }
+
+   @Patch(':id')
+   update(@Param('id') id: string, @Body() updatedUser: {}) {
+      return { id, ...updatedUser }
+   }
+
+   @Delete(':id')
+   delete(@Param('id') id: string) {
+      return `Delete user ${id}`
+   }
+}
